4c008b4363bcd4c7c275e68b2c86901caf50beaa
M-Momin <m.momin@students.ephec.be>
2023-05-06 17:52:54 +0200

    Updates

--- assets/Footer-623163ab.js
+++
@@ -1,1 +0,0 @@
-import{a as o}from"./index-c9c3b913.js";function r(){return o("div",{className:"footer-main-div"})}export{r as default};
---
+++ assets/Footer-97f2d6a7.js
@@ -0,0 +1,1 @@
+import{a as o}from"./index-aea67522.js";function r(){return o("div",{className:"footer-main-div"})}export{r as default};
