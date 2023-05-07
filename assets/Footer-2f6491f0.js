ddccf10089a1f7bde112ade8ec8b2e06f7af430e
M-Momin <m.momin@students.ephec.be>
2023-05-07 11:24:50 +0200

    Updates

--- assets/Footer-2f6491f0.js
+++
@@ -1,1 +0,0 @@
-import{a as o}from"./index-7b147f16.js";function r(){return o("div",{className:"footer-main-div"})}export{r as default};
---
+++ assets/Footer-76d206ec.js
@@ -0,0 +1,1 @@
+import{a as o}from"./index-9571e1bb.js";function r(){return o("div",{className:"footer-main-div"})}export{r as default};
